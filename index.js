5f54e0b95ec3edcb462e71d8541dbd70cfcee51e
Manoj Choudhury <aj.manoj99@gmail.com>
2021-07-22 15:45:22 +0000

    mathematics flow design

---
+++ index.js
@@ -0,0 +1,20 @@
+import { Design, DesignElement, FlowDesigns } from 'https://cdn.jsdelivr.net/gh/flowaboard/flowaboard/front-end/flowdesign/design.js';
+
+
+var matheMaticsDesign = new FlowDesigns.ListDesign('MatheMatics', 'mathematics', `https://en.wikipedia.org/wiki/matheMaticsion`)
+matheMaticsDesign.add(new DesignElement('Functions', 'function', `https://en.wikipedia.org/wiki/Function_(mathematics)`))
+matheMaticsDesign.add(new DesignElement('Equations', 'equation', `https://en.wikipedia.org/wiki/Computer_programming`))
+
+matheMaticsDesign.flowConfig = {
+    flex: true,
+    defaultValue: {
+        widthfactor: matheMaticsDesign.designElements.length,
+        xPadding: 0.4,
+        yPadding: 0.4,
+    },
+    action: {
+
+    }
+}
+
+export default matheMaticsDesign;

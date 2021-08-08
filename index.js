5c53eb7d07d34b3b3c39aea1fe34af2ed3a8e2b3
Manoj Choudhury <aj.manoj99@gmail.com>
2021-08-08 07:43:25 +0000

    Transfered mathematics designs from flow abord repo

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import { Design, DesignElement, FlowDesigns } from 'https://flowaboard.github.io/flowaboard/front-end/flowdesign/design.js';
 
 
-var matheMaticsDesign = new FlowDesigns.ListDesign('MatheMatics', 'mathematics', `https://en.wikipedia.org/wiki/matheMaticsion`)
-matheMaticsDesign.add(new DesignElement('Functions', 'function', `https://en.wikipedia.org/wiki/Function_(mathematics)`))
-matheMaticsDesign.add(new DesignElement('Equations', 'equation', `https://en.wikipedia.org/wiki/Computer_programming`))
+var matheMaticsDesign = new FlowDesigns.ListDesign('MatheMatics', 'mathematics', `https://en.wikipedia.org/wiki/Mathematics`)
+matheMaticsDesign.add(new DesignElement('Functions', 'function', `https://en.wikipedia.org/wiki/Function_(mathematics)`,'flow-info','functiondesign.js'))
+matheMaticsDesign.add(new DesignElement('Equations', 'equation', `https://en.wikipedia.org/wiki/Equation`,'flow-info','equationdesign.js'))
 
+matheMaticsDesign.root=location.href+'https://flowaboard.github.io/mathematics'
 matheMaticsDesign.flowConfig = {
     flex: true,
     defaultValue: {
@@ -12,8 +13,8 @@ matheMaticsDesign.flowConfig = {
         xPadding: 0.4,
         yPadding: 0.4,
     },
-    action: {
-
+    elementAction: {
+        "click" : {"action":"flow","state":"default"}
     }
 }
 

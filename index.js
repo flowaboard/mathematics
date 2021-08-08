f2c8dbd79ce3987ef7343a1003d9383175b871c5
Manoj Choudhury <aj.manoj99@gmail.com>
2021-08-08 07:53:44 +0000

    Updated design root

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import { Design, DesignElement, FlowDesigns } from 'https://flowaboard.github.io/flowaboard/front-end/flowdesign/design.js';
 
+class MathematicsDesign extends FlowDesigns.ListDesign{
 
-var matheMaticsDesign = new FlowDesigns.ListDesign('MatheMatics', 'mathematics', `https://en.wikipedia.org/wiki/Mathematics`)
+}
+
+var matheMaticsDesign = new MathematicsDesign('MatheMatics', 'mathematics', `https://en.wikipedia.org/wiki/Mathematics`)
 matheMaticsDesign.add(new DesignElement('Functions', 'function', `https://en.wikipedia.org/wiki/Function_(mathematics)`,'flow-info','/functiondesign.js'))
 matheMaticsDesign.add(new DesignElement('Equations', 'equation', `https://en.wikipedia.org/wiki/Equation`,'flow-info','/equationdesign.js'))
 
-matheMaticsDesign.root=location.href+'https://flowaboard.github.io/mathematics'
+matheMaticsDesign.root='https://flowaboard.github.io/mathematics'
 matheMaticsDesign.flowConfig = {
     flex: true,
     defaultValue: {
